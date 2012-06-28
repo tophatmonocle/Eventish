0e93672f2a44c7f074b0485ebd56545d3a0e1c06
Matt Hughes <hughesm02@gmail.com>
2012-06-27 21:25:52 -0400

    added template system

--- app.js
+++ app.js
@@ -3,15 +3,88 @@ var _ = require('underscore');
 var express = require('express');
 var mongo = require('mongodb');
 var http = require('http');
+var fs = require('fs');
 
 var app = express.createServer();
-
-app.get('/get', function(req, res) {
-	res.send('get');
+app.configure(function() {
+	app.use(express.bodyParser());
 });
 
-app.get('/put', function(req,res) {
-	res.send("put");
+var settings = {
+	appname: "eventish",
+	dbhost: "localhost",
+	dbport: 27017,
+	dbname: "eventish",
+}
+
+var events;
+
+var db = new mongo.Db(settings.dbname, new mongo.Server(settings.dbhost, settings.dbport, {}), {});
+db.open(function(err) {
+	if(err) {
+		console.log("database error");
+		process.exit(1)
+	}
+	console.log("connected to database");
+	db.collection("events", function(err, collection) {
+		if(err) {
+			console.log("collection error");
+			process.exit(1);
+		}
+		events = collection;
+	})
+})
+
+var main_page;
+fs.readFile('./templates/index.html', function(err, html) {
+	if(err) {
+		console.log("cannot load template");
+		process.exit(1);
+	}
+	main_page = html;
+})
+
+
+app.get('/', function(req, res) {
+	res.send(main_page);
+})
+
+app.get('/script/:filename', function(req, res) {
+	fs.readFile('./scripts/'+req.params.filename, function(err, js) {
+		if(err) {
+			res.send("Script not found");
+			return;
+		}
+		res.send(js);
+	}
+})
+
+app.get('/event', function(req, res) {
+	events.find({}, function(err, cursor) {
+		cursor.toArray(function(err, result) {
+			res.send(result)
+		})
+	})
+});
+
+app.post('/event', function(req,res) {
+	console.log(req.body);
+	console.log(req.body.tags);
+	if(!req.body.tags) {
+		res.send("at least one tag required");
+		return;
+	}
+	new_event = {
+		date: new Date(),
+		tags: req.body.tags
+	}
+	if(req.body.data) {
+		new_event.data
+	}
+	events.insert(new_event, function(err, result) {
+		console.log(result);
+		res.send(result._id)
+	})
 });
 
 app.listen(3100);
