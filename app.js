bdcaca6c5de9b30b94fd83ba4d8a2c4fddfc73f0
Matt Hughes <hughesm02@gmail.com>
2012-07-03 12:14:05 -0400

    converted to socket.io, added bootstrap

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 
 var _ = require('underscore');
-var express = require('express');
-var app = express.createServer();
-var io = require('socket.io').listen(app);
+var express = require('express')
+var sio = require('socket.io');
 var mongo = require('mongodb');
 var http = require('http');
 var fs = require('fs');
 var Buffer = require('buffer').Buffer;
+
+var app = express.createServer();
+
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname + '/static'))
+	app.set('views', __dirname + '/templates');
+	app.set('view engine', 'jade');
+});
+
+app.listen(3100);
+var io = sio.listen(app)
+
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/img', express.static(__dirname + '/img'))
 
 var generation = 0; // generation subscriber generation
 var seconds = 1000; // length of a second
@@ -19,8 +33,6 @@ var db_settings = {
 }
 
 var subscription_settings = {
-	host: "localhost",
-	port: 28018,
 	timeout: 60*seconds,
 	max_timeout: 2 // timeout periods before being removed
 }
@@ -56,10 +68,6 @@ db.open(function(err) {
 
 // EVENTS 
 
-app.configure(function() {
-	app.use(express.bodyParser());
-});
-
 app.get('/event', function(req, res) {
 	events.find(req.body, function(err, cursor) {
 		cursor.toArray(function(err, result) {
@@ -87,37 +95,15 @@ app.post('/event', function(req,res) {
 });
 
 app.get('/', function(req, res) {
-	fs.readFile('./templates/index.html', 'ascii', function(err, html) {
-		if(err) {
-			console.log("cannot load template");
-			process.exit(1);
-		}
-		res.send(html);
-	})
-})
-
-app.get('/scripts/:filename', function(req, res) {
-	fs.readFile('./scripts/'+req.params.filename, 'ascii', function(err, js) {
-		if(err) {
-			res.send("Script not found");
-			return;
-		}
-		res.send(js);
-	});
-})
-
-app.listen(3100);
+	res.render('index', { layout: false });
+});
 
 // SUBSCRIPTIONS
 
 io.sockets.on('connection', function(socket) {
-	socket.on('subscribe', function(tags) {
-		subscriptions.update({ socket: socket }, { socket: socket, tags: tags, generation: generation });
-	})
-});
-
-io.sockets.on('message', function(msg, callback) {
-	subscriptions.update({ from: from }, { from: from, tags: msg.tags, generation: generation }, { upsert: true });
+	socket.on('message', function(msg, callback) {
+		subscriptions.update({ socket: socket }, { socket:socket, tags: msg.tags, generation: generation }, { upsert: true });
+	});
 });
 
 var subscription_timeout = setInterval(function() {
